9be022121fa9379f903d524f7e8b3f64427fe8d8
Maksio <jozef@gorowo.pl>
2025-09-08 23:44:26 +0200

    Drobne kroczki 1 - Przycisk zmienia swoją nazwę
    onPropertyChanged
    Binding

--- Connector/ViewModels/LoginWindowViewModel.cs
+++ Connector/ViewModels/LoginWindowViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace Connector.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Connector.ViewModels
 {
     internal class LoginWindowViewModel : ViewModelBase
     {
+        private string _loginButtonText = "Zaloguj";
+
+        public string LoginButtonText
+        {
+            get { return _loginButtonText; }
+            set { _loginButtonText = value; }
+        }
+
+        public void LoginButtonClick()
+        {
+            LoginButtonText = "Zalogowano";
+            OnPropertyChanged(nameof(LoginButtonText));
+        }
     }
 }
